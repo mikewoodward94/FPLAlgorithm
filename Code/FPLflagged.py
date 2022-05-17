96ef6dbbedab2b9b2ac1b199dcef1f95fb8b2949
McSpoish <105747447+McSpoish@users.noreply.github.com>
2022-05-17 20:31:14 +0100

    Add FPLflagged.py ad FPLtimiser.py

---
+++ Code/FPLflagged.py
@@ -0,0 +1,92 @@
+import requests
+import pandas as pd
+
+FLAGGEDLIST = r'C:\Users\McSpo\.spyder-py3\FPL\Data\flagged_players.csv'
+
+def fpl_flagged():
+    '''
+    Creates a list of players who were flagged by gameweek(gw)/event/round.
+    This is so that they can be removed from the historic data so players returning
+    from injury, injured in game etc aren't unfairly penalised for being flagged.
+
+    Returns:
+        Dataframe: element, round
+
+    '''
+    flagged_list = pd.read_csv(FLAGGEDLIST)
+
+    current_status = get_current_player_status()
+    current_flagged_players = current_status.loc[current_status['status'].isin(['i','d'])]
+
+    players = get_current_gw_player_info()
+
+    current_flagged_players = pd.merge(players, current_flagged_players, left_on=['element'], right_on=['id'], how='right').drop('id',1)
+    current_flagged_players = current_flagged_players.loc[current_flagged_players['minutes'] < 45]
+    current_flagged_players = current_flagged_players[['element','round']].drop_duplicates()
+
+    flagged_players = pd.concat([flagged_list, current_flagged_players]).drop_duplicates()
+
+    flagged_players.to_csv(r'C:\Users\McSpo\.spyder-py3\FPL\Data\flagged_players.csv', index = None)
+
+def get_current_player_status():
+    '''
+    Retrieves current status of each player
+    Returns:
+        Dataframe: element, status
+    '''
+    url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    r = requests.get(url)
+    json = r.json()
+    elements_df = pd.DataFrame(json['elements'])
+    status = elements_df[['id', 'status']]
+
+    return(status)
+
+def get_current_gw_player_info():
+    '''
+    Retrieves information from the current gw
+    Returns:
+        Dataframe: element, event, minutes
+    '''
+    no_of_players = get_no_of_players()
+    player_data = []
+
+    for i in range(1, no_of_players + 1):
+        p_url = "https://fantasy.premierleague.com/api/element-summary/"+str(i)+"/"
+        rp = requests.get(p_url)
+        jsonp = rp.json()
+        data = pd.DataFrame(jsonp['history'])
+        data = data[['element', 'round', 'minutes']]
+        player_data.append(data)
+
+    player_data=pd.concat(player_data)
+
+    gw = get_current_gw()
+    players = player_data.loc[player_data['round'] == gw]
+
+    return(players)
+
+def get_current_gw():
+    '''
+    Returns: Current gameweek/event
+    '''
+    url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    r = requests.get(url)
+    json = r.json()
+    events_df = pd.DataFrame(json['events'])
+    current_gw = events_df.loc[events_df['is_current'] == True]['id'].iloc[0]
+
+    return(current_gw)
+
+def get_no_of_players():
+    '''
+    Returns the number of football players with data for this season.
+    '''
+    url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    r = requests.get(url)
+    json = r.json()
+    elements = pd.DataFrame(json['elements'])
+    return(len(elements))
+
+if __name__ == '__main__':
+    fpl_flagged()
---
+++ Code/FPLtimiser.py
@@ -0,0 +1,224 @@
+import pandas as pd
+import pulp
+
+def fpl_optimiser(projected_scores_for_optimiser, transfers, in_bank):
+    '''
+    Creates excel workbook of optimal squad/starters/captain for each week with FPL transfer logic.
+    '''
+    data = projected_scores_for_optimiser
+    
+    player_ids = data.index
+    
+    weeks = len(data.columns[5:-25])    
+    free_transfers = transfers
+    bank = in_bank
+    columns = data.columns[5:-25]
+    
+    squad_var = {i: {} for i in range(1,weeks+1)}
+    start_var = {i: {} for i in range(1,weeks+1)}
+    cap_var = {i: {} for i in range(1,weeks+1)}
+    both_var = {i: {} for i in range(1, weeks+1)}
+    week = {i: {} for i in range(1,weeks+1)}
+    a_transfers_var = {i: [] for i in range(1,weeks+1)}
+    in_team = data['in_team']
+    cost = data['now_cost']
+    GK = data['GK']
+    DEF = data['Def']
+    MID = data['Mid']
+    ATT = data['Att']
+    team1 = data['team1']
+    team2 = data['team2']
+    team3 = data['team3']
+    team4 = data['team4']
+    team5 = data['team5']
+    team6 = data['team6']
+    team7 = data['team7']
+    team8 = data['team8']
+    team9 = data['team9']
+    team10 = data['team10']
+    team11 = data['team11']
+    team12 = data['team12']
+    team13 = data['team13']
+    team14 = data['team14']
+    team15 = data['team15']
+    team16 = data['team16']
+    team17 = data['team17']
+    team18 = data['team18']
+    team19 = data['team19']
+    team20 = data['team20']
+    
+    #vars
+    for a in range(1,weeks+1):
+        a_transfers_var[a] = pulp.LpVariable("wk" + str(a) + "at",lowBound=0, upBound=2, cat='integer')
+        for i in player_ids:
+            squad_var[a][i] = pulp.LpVariable('wk' + str(a) + 'x' + str(i), cat='Binary')
+            start_var[a][i] = pulp.LpVariable('wk' + str(a) + 'y' + str(i), cat='Binary')
+            cap_var[a][i] = pulp.LpVariable('wk' + str(a) + 'z' + str(i), cat='Binary')
+            both_var[a][i] = pulp.LpVariable('wk' + str(a) + '+' + 'wk' + str(a+1) + 'b' + str(i), cat='Binary')
+            week[a][i] = data[columns[a-1]][i]
+        
+    prob = pulp.LpProblem("Optimiser", pulp.LpMaximize)
+    
+    #Objective
+    prob += pulp.lpSum([((week[a][i] * start_var[a][i]) + (week[a][i] * cap_var[a][i]) ) for a in range(1,weeks+1) for i in player_ids])
+    
+    #15 in squad and 11 in starters and 1 captain, starters in squad, captain in starters
+    for a in range(1,weeks+1):
+        prob += pulp.lpSum([squad_var[a][i] for i in player_ids]) == 15
+        prob += pulp.lpSum([start_var[a][i] for i in player_ids]) == 11
+        prob += pulp.lpSum([cap_var[a][i] for i in player_ids]) == 1
+        prob += pulp.lpSum([both_var[a][i] for i in player_ids]) >= 13
+        '''
+        prob += pulp.lpSum(squad_var[a][232]) == 1
+        prob += pulp.lpSum(squad_var[a][236]) == 1
+        prob += pulp.lpSum(squad_var[a][144]) == 1
+        prob += pulp.lpSum(start_var[a][232]) == 1
+        prob += pulp.lpSum(start_var[a][236]) == 1
+        '''
+        for i in player_ids:
+            prob += start_var[a][i] <= squad_var[a][i]
+            prob += cap_var[a][i] <= start_var[a][i]
+    
+    #week 1 only different to current team by max FT
+    prob += pulp.lpSum([squad_var[1][i] * in_team[i] for i in player_ids]) >= 15 - free_transfers
+    
+    #initial amount of transfers is set accounting for wildcard
+    if free_transfers == 15:
+        prob += a_transfers_var[1] == 1
+    else:
+        prob += a_transfers_var[1] <= free_transfers + 1 - 15 + pulp.lpSum([squad_var[1][i] * in_team[i] for i in player_ids])
+    
+    #amount of transfers available can't be 0 after initial
+    for a in range(1,weeks+1):
+        prob += a_transfers_var[a] >= 1
+    
+    #transfer rules(max 2 a week, accrue 1 a week + starting n) + limit no. of changes to available transfers
+    for a in range(1,weeks):
+        prob += a_transfers_var[a+1] <= a_transfers_var[a] + 1 - 15 + pulp.lpSum([both_var[a][i] for i in player_ids])
+        prob += 15 - pulp.lpSum([both_var[a][i] for i in player_ids]) <= a_transfers_var[a]
+    
+        for i in player_ids:
+            prob += squad_var[a+1][i] >= both_var[a][i]
+            prob += squad_var[a][i] >= both_var[a][i]
+            
+        prob += pulp.lpSum(
+        [15 - pulp.lpSum([both_var[b][i] for i in player_ids])
+        for b in range(1, a+1)]) <= a - 1 + a_transfers_var[1]
+    
+    #cost can't be more than current value + bank
+    for a in range(1,weeks+1):
+        prob += pulp.lpSum([squad_var[a][i] * cost[i] for i in player_ids]) <= sum([in_team[i] * cost[i] for i in player_ids]) + bank
+    
+    #squad has 2 gk, 5 def, 5 mid, 3 att, starting team has 1 gk, 3-5 def, 2-5 mid, 1-3 att
+    for a in range(1,weeks+1):
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] for i in player_ids]) == 2
+        prob += pulp.lpSum([squad_var[a][i] * DEF[i] for i in player_ids]) == 5
+        prob += pulp.lpSum([squad_var[a][i] * MID[i] for i in player_ids]) == 5
+        prob += pulp.lpSum([squad_var[a][i] * ATT[i] for i in player_ids]) == 3
+        
+        prob += pulp.lpSum([start_var[a][i] * GK[i] for i in player_ids]) == 1
+        prob += pulp.lpSum([start_var[a][i] * DEF[i] for i in player_ids]) >= 3
+        prob += pulp.lpSum([start_var[a][i] * DEF[i] for i in player_ids]) <= 5
+        prob += pulp.lpSum([start_var[a][i] * MID[i] for i in player_ids]) >= 2
+        prob += pulp.lpSum([start_var[a][i] * MID[i] for i in player_ids]) <= 5
+        prob += pulp.lpSum([start_var[a][i] * ATT[i] for i in player_ids]) >= 1
+        prob += pulp.lpSum([start_var[a][i] * ATT[i] for i in player_ids]) <= 3
+    
+    #squad has max of 3 players of any PL team
+    for a in range(1,weeks+1):
+        prob += pulp.lpSum([squad_var[a][i] * team1[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team2[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team3[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team4[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team5[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team6[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team7[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team8[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team9[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team10[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team11[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team12[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team13[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team14[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team15[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team16[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team17[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team18[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team19[i] for i in player_ids]) <= 3
+        prob += pulp.lpSum([squad_var[a][i] * team20[i] for i in player_ids]) <= 3
+        
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team1[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team1[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team2[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team2[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team3[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team3[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team4[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team4[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team5[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team5[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team6[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team6[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team7[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team7[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team8[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team8[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team9[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team9[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team10[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team10[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team11[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team11[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team12[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team12[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team13[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team13[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team14[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team14[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team15[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team15[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team16[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team16[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team17[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team17[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team18[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team18[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team19[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team19[i] for i in player_ids]) <= 2
+        prob += pulp.lpSum([squad_var[a][i] * GK[i] * team20[i] for i in player_ids]) + pulp.lpSum([squad_var[a][i] * DEF[i] * team20[i] for i in player_ids]) <= 2
+    
+    prob.solve(pulp.GLPK_CMD(timeLimit=900))
+    print(pulp.LpStatus[prob.status])
+    
+    #TO DO WORK ON OUTPUT
+    
+    # Output for checking/testing
+    
+    output = data[['element', 'name', 'now_cost', 'in_team']]
+    
+    squad = {i: {} for i in range(1,weeks+1)}
+    start = {i: {} for i in range(1,weeks+1)}
+    cap = {i: {} for i in range(1,weeks+1)}
+    
+    squad_col = []
+    start_col = []
+    cap_col = []
+    
+    for a in range(0, weeks):
+        squad_col.append('squad_' + columns[a])
+        start_col.append('start_' + columns[a])
+        cap_col.append('cap_' + columns[a])
+    
+    for a in range(1,weeks+1):
+        for i in player_ids:
+            squad[a][i] = squad_var[a][i].varValue
+            start[a][i] = start_var[a][i].varValue
+            cap[a][i] = cap_var[a][i].varValue
+    
+    squad_df = pd.DataFrame(squad)
+    squad_df.columns = squad_col
+    squad_df['element'] = output['element'] 
+    squad_df = squad_df.loc[(squad_df.iloc[:,:-1] != 0).any(axis=1)]
+    squad_df = pd.merge(output, squad_df, on=['element'], how='right')
+    
+    start_df = pd.DataFrame(start)
+    start_df.columns = start_col
+    start_df['element'] = output['element'] 
+    start_df = start_df.loc[(start_df.iloc[:,:-1] != 0).any(axis=1)]
+    start_df = pd.merge(output, start_df, on=['element'], how='right')
+    
+    cap_df = pd.DataFrame(cap)
+    cap_df.columns = cap_col 
+    cap_df['element'] = output['element'] 
+    cap_df = cap_df.loc[(cap_df.iloc[:,:-1] != 0).any(axis=1)]
+    cap_df = pd.merge(output, cap_df, on=['element'], how='right')
+    
+    writer = pd.ExcelWriter(r'C:\Users\McSpo\.spyder-py3\FPL\Data\chosen_teams.xlsx', engine = 'xlsxwriter')
+    squad_df.to_excel(writer, sheet_name = 'Squad', index=False)
+    start_df.to_excel(writer, sheet_name = 'Start', index=False)
+    cap_df.to_excel(writer, sheet_name = 'Captain', index=False)
+    writer.save()
+    
+if __name__ == '__main__':
+    fpl_optimiser()
